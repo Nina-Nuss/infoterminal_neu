96adc2042c332450671c2463f1bc446c1bb1ffeb
nina.nuss <nina.nuss01@gmail.com>
2025-11-25 10:09:28 +0100

    .

--- src/test/testJson/saveValue.php
+++ src/test/testJson/saveValue.php
@@ -2,10 +2,23 @@
 $data = json_decode(file_get_contents('php://input'), true);
 $key = $data['key'] ?? "test";
 $value = $data['value'] ?? "test";
+
 echo json_encode("Key: " . $key . " Value: " . $value . "\n");
+
 $jsonData = file_get_contents('../../../config/configTest.json');
 $config = json_decode($jsonData, true);
-// $key = $data['key'];
-// $value = $data['value'];
+
+
+foreach ($data as $item) {
+    $key = $item['key'];
+    $value = $item['value'];
+}
+
+
+
 $config['webpageSettings'][0][$key] = $value;
+
+
+
 file_put_contents('../../../config/configTest.json', json_encode($config, JSON_PRETTY_PRINT));
+echo json_encode("Key: " . $key . " Value: " . $value . "\n");
